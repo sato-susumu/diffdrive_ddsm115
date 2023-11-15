a4e110b671fbcb31d4137d41826cc2bb0ed3398a
kenmacken <kenmacken@gmail.com>
2023-11-16 07:38:45 +1100

    Right wheel - inverted direction of spin

--- hardware/diffbot_system.cpp
+++ hardware/diffbot_system.cpp
@@ -221,7 +221,7 @@ hardware_interface::return_type DiffDriveDDSM115Hardware::read(
   wheel_pos = commsDDSM_.responseData.angle;
   wheel_vel = commsDDSM_.responseData.velocity;
 
-  wheel_r_.pos_rads = wheel_r_.degrees_to_radians(wheel_r_.calculate_accumulated_position(wheel_pos));
+  wheel_r_.pos_rads = wheel_r_.degrees_to_radians(-wheel_r_.calculate_accumulated_position(wheel_pos));
   wheel_r_.vel = wheel_r_.rpm_to_rad_per_sec(wheel_vel);
   // RCLCPP_INFO(rclcpp::get_logger("DiffDriveDDSM115Hardware"), "WR Position is: %f", wheel_r_.pos);
   // RCLCPP_INFO(rclcpp::get_logger("DiffDriveDDSM115Hardware"), "WR Velocity is: %f", wheel_r_.vel);
