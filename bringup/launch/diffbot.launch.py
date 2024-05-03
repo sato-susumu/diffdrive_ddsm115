7d8b73e92772d0ecbf4c84886c057a2fa1f226f2
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2024-05-03 12:31:41 +0900

    update launch file

--- bringup/launch/diffbot.launch.py
+++ bringup/launch/diffbot.launch.py
@@ -56,9 +56,6 @@ def generate_launch_description():
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
-        remappings=[
-            ("/diff_drive_controller/cmd_vel", "/cmd_vel"),
-        ],
     )
     rviz_node = Node(
         package="rviz2",
